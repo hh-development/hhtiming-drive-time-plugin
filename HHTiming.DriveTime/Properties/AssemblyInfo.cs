f45bbb39baf0c1196b091eb6f8c1c9a3411cae74
David Funk <david@hh-dev.com>
2025-09-03 09:01:12 +0200

    update to major 2025

--- HHTiming.DriveTime/Properties/AssemblyInfo.cs
+++ HHTiming.DriveTime/Properties/AssemblyInfo.cs
@@ -1,6 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -34,3 +35,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.9.2")]
 [assembly: AssemblyFileVersion("0.9.2")]
+[assembly: SupportedOSPlatform("windows")]
